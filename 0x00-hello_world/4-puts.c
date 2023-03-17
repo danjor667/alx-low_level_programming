d5a564e23b1758ae8f620e9dd35c54600ba76359
danjor667 <jordannguepi@gmail.com>
2023-03-17 14:39:26 -0700

    c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+ * initiating the main funcion
+ * Return:0
+ */
 int main(void)
 {
-	char string[] = "\"Programming is like building a multilingual puzzle";
-	puts(string);
-	return 0;
+char string[] = "\"Programming is like building a multilingual puzzle";
+puts(string);
+return 0;
 }
