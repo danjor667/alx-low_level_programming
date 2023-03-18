921fbf5d0671df087e9dc5b9e02db20827b05ed2
danjor667 <jordannguepi@gmail.com>
2023-03-18 07:16:44 -0700

    int

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A function that print number using the putchar function
+ * Return: (0)
+ */
+int main(void)
+{
+int start = 0;
+
+for (start = 0; start <= 9; start++)
+	putchar(start);
+return (0);
+}
+
