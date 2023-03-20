a20e50a2a6b25a1d3bf04dc2d1de356ac999b256
danjor667 <jordannguepi@gmail.com>
2023-03-20 13:06:53 -0700

    sigle digit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,10 +5,13 @@
  */
 int main(void)
 {
-int start = 0;
+	int start = 0;
 
-for (start = 0; start <= 9; start++)
-	putchar(start);
-return (0);
+	for (start = 0; start <= 9; start++)
+	{
+		putchar('0' + start);
+	}
+	putchar('\n');
+	return (0);
 }
 
