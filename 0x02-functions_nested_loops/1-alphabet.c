e3b23d9f2b15cf98fa0c99f317f3c7eb541f1db1
danjor667 <jordannguepi@gmail.com>
2023-03-22 14:40:17 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - a function that printd the alphabet
+ */
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
