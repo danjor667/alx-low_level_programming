b85ed54b5ac5f352c3cf15c6a5bc41253d6b5e05
danjor667 <jordannguepi@gmail.com>
2023-03-28 17:51:07 -0700

    reversing function

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -20,8 +20,10 @@ void rev_string(char *s)
 	j = len - 1;
 	while (i != j)
 	{
-		temp = *s[i];
-		*s[i] = *s[j];
-		*s[j] = temp;
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		i++;
+		j--;
 	}
 }
