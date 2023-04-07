5c531623d18418ae38516fca1e240000feb4f391
danjor667 <jordannguepi@gmail.com>
2023-04-06 19:14:41 -0700

    task 5 update

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _sqrt_recursion - a function that calculate the natural 
+ * _sqrt_recursion - a function that calculate the natural
  * square root of a number
  * @n: input number
  * Return: -1 if natural squaroot dont exist
  */
 int _sqrt_recursion(int n)
 {
-	return sqr(n, 0, n);
+	return (sqr(n, 0, n));
 }
 /**
  * sqr - a helper function
@@ -24,28 +24,33 @@ int sqr(int n, int start, int  end)
 	}
 	else if (n == 1 || n == 0)
 	{
-        	return (n);
+		return (n);
 	}
 	else
 	{
-		int mp = start + (end - start)/2;
-	
+		int mp = start + (end - start) / 2;
+
 		if (mp * mp == n)
 		{
 			return (mp);
 		}
 		else if (mp * mp > n)
 		{
-			return  sqr(n, 0, mp - 1);
-			/* (mp -1) to adjust the range, to prevent infinite recurssion like for n = 25*/
+			return  (sqr(n, 0, mp - 1));
+			/**
+			 * (m-1) here  or (m+1)in line 49
+			 * is to adjust the range
+			 * to prevent infinite recurssion
+			 * like for when n = 25
+			 */
 		}
-		else if ((mp +1) * (mp +1) > n)
+		else if ((mp + 1) * (mp + 1) > n)
 		{
 			return (-1);
 		}
 		else
 		{
-			return sqr(n, mp + 1, n);
+			return (sqr(n, mp + 1, n));
 		}
 	}
 }
