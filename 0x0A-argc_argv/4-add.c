6e01d058deded741ef5aad417918f48a6746f864
Ubuntu <ubuntu@ip-172-31-6-86.eu-north-1.compute.internal>
2023-04-08 14:43:32 +0000

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - entry point of the programm
+ * @argc: input
+ * @argv: input
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i = 1;
+
+	if (argc == 1)
+	{
+		printf("%d\n", sum);
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (isdigit(argv[i][0]))
+		{
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
