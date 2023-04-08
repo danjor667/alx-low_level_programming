a215e4b9fb605f3bf25bbf2d87d33c12240c6c18
danjor667 <jordannguepi@gmail.com>
2023-04-07 18:19:52 -0700

    task 1 updated

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "main.h"
-/** main - the entry point
- * @argc: input argument 
+/**
+ * main - the entry point
+ * @argc: input argument
  * @argv: input argument
  * Return: int
  */
