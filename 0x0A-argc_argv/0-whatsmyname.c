d637a7d6b3000b806c624bb99732b95e6949255f
danjor667 <jordannguepi@gmail.com>
2023-04-07 18:08:33 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry function
+ * @argc: argument count input
+ * @argv: argument vector input
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
