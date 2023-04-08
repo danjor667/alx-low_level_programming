4d9a31fa37ec097b2857ea8b74f9a74e7c4f9cb0
danjor667 <jordannguepi@gmail.com>
2023-04-07 19:00:15 -0700

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - entry function
+ * @argc: input
+ * @argv: input
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	sum = atoi(argv[1]) * atoi(argv[2]);
+	if (argc != 3)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		printf("%d\n", sum);
+	}
+	return (0);
+}
