a9a55b9cf5def69232ad8a9e411596550a25e441
danjor6.77 <jordannguepi@gmail.com>
2023-04-20 00:52:34 +0000

    task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+/**
+ * op_add- a function to add 2 int
+ * @a:inut int
+ * @b:input int
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub- func to find the diff of 2 int
+ * @a: input int
+ * @b: input int
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul- function to multiply 2 numbers
+ * @a: input
+ * @b: input int
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div- funct to divide 2 numbers
+ * @a: input
+ * @b: input
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod- funct to find the modulus of 2 num
+ * @a: input
+ * @b: input
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
