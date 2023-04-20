21af0e04cf09bbe43347cc7a4d609ca22aa99d78
danjor6.77 <jordannguepi@gmail.com>
2023-04-20 01:20:25 +0000

    task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include <string.h>
+#include <stdlib.h>
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * main- entry point of the program
+ * @argc: number of input items
+ * @argv: array of input element
+ * Return: int
+ */
+int main(argc, **argv)
+{
+	char *op = argv[2];
+	int (*used_function)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (strlen(argv[2] != 1 || used_function == NULL))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (atoi(argv[3]) == 0 && (*op == "%" || *op == "/"))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", used_fuction(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
