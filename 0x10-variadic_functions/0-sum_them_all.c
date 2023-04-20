a3048b507be5fc65a9a7913dc1b1167cdca837bf
danjor667 <jordannguepi@gmail.com>
2023-04-20 04:38:15 -0700

    task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all- function to evaluate the sum of 
+ * a given set of numbers
+ * @n: numbers of element to be summed up
+ * Return: int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	va_list args;
+	unsigned int i;
+	va_start(args, n);
+	
+	for (i = 0; i < n; i++)
+	{
+		int x = va_arg(args, int);
+		sum += x;
+	}
+	va_end(args);
+	return (sum);
+}
