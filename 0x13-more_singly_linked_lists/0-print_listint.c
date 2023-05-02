adcd573b6db40d522dece74847203354ff6940bf
danjor667 <jordannguepi@gmail.com>
2023-05-02 15:03:52 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ #include "lists.h"
+ /** 
+  *print_listint - a functionthat prints the all the nodes of z list
+  *@h: pointer to yhe head of linked list
+  *Return: An integer of the number of noddd
+  */
+ size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		i++;
+		h = h->next
+	}
+	return (i);
+ }
